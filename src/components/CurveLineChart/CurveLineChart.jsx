5c3fb4a3a69bb42e517668fe8d3ae602efa6d458
deepak <guptadeepak2310@gmail.com>
2023-11-18 13:49:32 +0530

    Covid Cases Api Integeration

--- src/components/CurveLineChart/CurveLineChart.jsx
+++ src/components/CurveLineChart/CurveLineChart.jsx
@@ -1,74 +1,100 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
 
-export default function CurveLineChart({ width, height, data }) {
-  const svgHeight = height;
-  const svgWidth = width;
-  const paddingLeft = 10;
-  const paddingRight = 10;
-  const paddingBottom = 20;
+export default function CurveLineChart({ width, height, filteredGraphData }) {
+  const svgRef = useRef();
 
-  const svgRef = React.useRef();
-
-  React.useEffect(() => {
+  useEffect(() => {
     const svg = d3.select(svgRef.current);
+    svg.selectAll("*").remove();
+
+    const margin = { top: 10, right: 45, bottom: 20, left: 25 };
+    const innerWidth = width - margin.left - margin.right;
+    const innerHeight = height - margin.top - margin.bottom;
 
     const xScale = d3
+      .scaleTime()
+      .domain(d3.extent(filteredGraphData, (d) => d.date))
+      .range([0, innerWidth]);
+
+    const yScaleConfirmedRecovered = d3
       .scaleLinear()
-      .domain([0, data.length - 1])
-      .range([paddingLeft, svgWidth - paddingRight]);
+      .domain([
+        0,
+        d3.max(filteredGraphData, (d) => Math.max(d.confirmed, d.recovered)),
+      ])
+      .range([innerHeight, 0]);
 
-    const maxValue = Math.max(...data);
-    const highestYValue = svgHeight - maxValue + paddingBottom;
-    const yScale = d3
+    const yScaleDeceased = d3
       .scaleLinear()
-      .domain([0, highestYValue])
-      .range([svgHeight - paddingBottom, 0]);
-    const xAxis = d3
-      .axisBottom(xScale)
-      .ticks(data.length)
-      .tickFormat((index) => index + 1);
+      .domain([0, d3.max(filteredGraphData, (d) => d.deceased)])
+      .range([innerHeight, 0]);
 
-    svg
-      .select(".x-axis")
-      .style("transform", ` translateY(${svgHeight - paddingBottom}px)`)
-      .call(xAxis)
-      .call((g) => g.select(".domain").remove())
-      .call((g) => g.selectAll(".tick line").remove())
-      .selectAll("text")
-      .style("font-size", "14px")
-      .style("fill", "#c4c7c9");
+    const xAxis = d3.axisBottom(xScale).tickFormat(d3.timeFormat("%m-%Y"));
+    const yAxisConfirmedRecovered = d3
+      .axisLeft(yScaleConfirmedRecovered)
+      .tickSize(0)
+      .tickPadding(10);
+    const yAxisDeceased = d3
+      .axisRight(yScaleDeceased)
+      .tickSize(0)
+      .tickPadding(10);
 
-    const yAxis = d3.axisRight(yScale).tickSize(0);
-    svg
-      .select(".y-axis")
-      .style("transform", ` translateX(${svgWidth - paddingRight}px)`)
-      .call(yAxis)
-      .call((g) => g.select(".domain").remove())
-      .call((g) => g.selectAll(".tick text").remove());
+    const g = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    const myLine = d3
+    g.append("g").attr("transform", `translate(0,${innerHeight})`).call(xAxis);
+
+    g.append("g").call(yAxisConfirmedRecovered);
+
+    g.append("g")
+      .attr("transform", `translate(${innerWidth},0)`)
+      .call(yAxisDeceased);
+
+    const lineConfirmed = d3
       .line()
-      .x((value, index) => xScale(index))
-      .y(yScale)
-      .curve(d3.curveCardinal);
-    svg
-      .selectAll(".line")
-      .data([data])
-      .join("path")
-      .attr("class", "line")
-      .attr("d", myLine)
+      .x((d) => xScale(d.date))
+      .y((d) => yScaleConfirmedRecovered(d.confirmed))
+      .curve(d3.curveBasis);
+
+    const lineRecovered = d3
+      .line()
+      .x((d) => xScale(d.date))
+      .y((d) => yScaleConfirmedRecovered(d.recovered))
+      .curve(d3.curveBasis);
+
+    const lineDeceased = d3
+      .line()
+      .x((d) => xScale(d.date))
+      .y((d) => yScaleDeceased(d.deceased))
+      .curve(d3.curveBasis);
+
+    g.append("path")
+      .datum(filteredGraphData)
       .attr("fill", "none")
-      .attr("stroke", "#4800f0");
-  }, [data, svgHeight, svgWidth]);
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", lineConfirmed);
+
+    g.append("path")
+      .datum(filteredGraphData)
+      .attr("fill", "none")
+      .attr("stroke", "orange")
+      .attr("stroke-width", 1.5)
+      .attr("d", lineRecovered);
+
+    g.append("path")
+      .datum(filteredGraphData)
+      .attr("fill", "none")
+      .attr("stroke", "grey")
+      .attr("stroke-width", 1.5)
+      .attr("d", lineDeceased);
+  }, [height, width, filteredGraphData]);
 
   return (
     <React.Fragment>
-      <svg width={svgWidth} height={svgHeight} ref={svgRef}>
-        <g className="x-axis" />
-        <g className="y-axis" />
-      </svg>
-      <br />
+      <svg width={width} height={height} ref={svgRef} />
     </React.Fragment>
   );
 }
--- src/views/GraphViews/graphViews/CheckingAccount/CheckingAccountView.jsx
+++ src/views/GraphViews/graphViews/CheckingAccount/CheckingAccountView.jsx
@@ -1,25 +1,59 @@
-import React, { useState } from "react";
-import Select from "@mui/material/Select";
-import MenuItem from "@mui/material/MenuItem";
+import React, { useEffect, useState } from "react";
 import GraphCardView from "../../../../components/GraphCardView/GraphCardView";
 import CurveLineChart from "../../../../components/CurveLineChart/CurveLineChart";
 import GraphCardHeader from "../../../../components/GraphCardHeader/GraphCardHeader";
-import { generateCurvedGraphData } from "../../../../constants/constants";
+import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
+import { LocalizationProvider } from "@mui/x-date-pickers-pro";
+import { AdapterDayjs } from "@mui/x-date-pickers-pro/AdapterDayjs";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import "./CheckingAccount.css";
 
-export default function CheckingAccountView({ data, setCurvedGraphData }) {
-  const [month, setMonth] = useState("March");
+export default function CheckingAccountView() {
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+  const [graphData, setGraphData] = useState([]);
+  const [curvedLineData, setCurvedLineData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const onMonthSelectHandler = (e) => {
-    setMonth(e.target.value);
-    const curvedGraphData = generateCurvedGraphData(6);
-    setCurvedGraphData((prev) => {
-      return {
-        ...prev,
-        curvedLineData: curvedGraphData,
-      };
-    });
+  const checkValidDate = (date, sDate, eDate) => {
+    const givenDate = new Date(date);
+    const start = new Date(sDate);
+    const end = new Date(eDate);
+    return givenDate >= start && givenDate <= end;
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      setGraphData([]);
+      const response = await fetch(
+        "https://data.covid19india.org/v4/min/timeseries.min.json"
+      );
+      const result = await response.json();
+      const firstKey = Object.keys(result)[0];
+      const firstValue = result[firstKey];
+
+      const data = Object.entries(firstValue?.dates).map(([date, value]) => {
+        return {
+          date: new Date(date),
+          confirmed: value?.delta?.confirmed || 0,
+          recovered: value?.delta?.recovered || 0,
+          deceased: value?.delta?.deceased || 0,
+        };
+      });
+      const filteredGraphData = data?.filter((entry) =>
+        checkValidDate(entry?.date, startDate, endDate)
+      );
+      setGraphData(filteredGraphData);
+      setIsLoading(false);
+    };
+    if (startDate && endDate) fetchData();
+  }, [startDate, endDate]);
+
+  useEffect(() => {
+    if (graphData?.length > 0) setCurvedLineData(graphData);
+    else setCurvedLineData([]);
+  }, [graphData]);
 
   return (
     <GraphCardView
@@ -27,22 +61,44 @@ export default function CheckingAccountView({ data, setCurvedGraphData }) {
         <GraphCardHeader
           leftSectionText={"Checking account"}
           rightSection={
-            <Select
-              value={month}
-              onChange={onMonthSelectHandler}
-              displayEmpty
-              inputProps={{ "aria-label": "Without label" }}
-              className="my-select"
-            >
-              <MenuItem value={"January"}>January</MenuItem>
-              <MenuItem value={"February"}>February</MenuItem>
-              <MenuItem value={"March"}>March</MenuItem>
-              <MenuItem value={"April"}>April</MenuItem>
-            </Select>
+            <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={["DateRangePicker"]}>
+                  <DatePicker
+                    label="Start Date"
+                    value={startDate}
+                    onChange={(newValue) => setStartDate(newValue)}
+                  />
+                </DemoContainer>
+              </LocalizationProvider>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={["DateRangePicker"]}>
+                  <DatePicker
+                    label="End Date"
+                    value={endDate}
+                    onChange={(newValue) => setEndDate(newValue)}
+                  />
+                </DemoContainer>
+              </LocalizationProvider>
+            </div>
           }
         />
       }
-      BottomComponent={<CurveLineChart width={600} height={200} data={data} />}
+      BottomComponent={
+        !startDate && !endDate && curvedLineData?.length === 0 ? (
+          isLoading ? (
+            <p>Loading....</p>
+          ) : (
+            <p>Please select the start and end dates</p>
+          )
+        ) : (
+          <CurveLineChart
+            width={600}
+            height={200}
+            filteredGraphData={curvedLineData}
+          />
+        )
+      }
     />
   );
 }
